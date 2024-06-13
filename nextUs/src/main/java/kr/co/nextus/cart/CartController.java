b2512f38f7864160afe32720a2bf17d49bd142ad
genie-Ryu <69509924+genie-Ryu@users.noreply.github.com>
2024-06-13 14:49:20 +0900

    장바구니 1차( 장바구니 db저장, 조회, 삭제)까지 완료.

---
+++ nextUs/src/main/java/kr/co/nextus/cart/CartController.java
@@ -0,0 +1,47 @@
+package kr.co.nextus.cart;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.nextus.member.MemberVO;
+
+@Controller
+public class CartController {
+	
+	@Autowired
+	private CartService service;
+	
+	@GetMapping("/cart/cart.do")
+	public String cart(Model model, HttpServletRequest request, @RequestParam("sellno") int sellno, @RequestParam("optionno") int optionno) {
+		HttpSession sess = request.getSession();
+		MemberVO login = (MemberVO)sess.getAttribute("login");
+		int memberno = login.getNo();
+		int r = service.insert(memberno, sellno, optionno);
+		List<CartVO> list = service.list(memberno, sellno, optionno);
+		model.addAttribute("list",list);
+		return "/cart/cart";
+	}
+	
+	@PostMapping("/cart/delete.do")
+    @ResponseBody
+    public String delete(HttpServletRequest request, @RequestParam("sellno") int sellno, @RequestParam("optionno") int optionno) {
+        HttpSession sess = request.getSession();
+        MemberVO login = (MemberVO) sess.getAttribute("login");
+        
+        int memberno = login.getNo();
+        
+        int r = service.delete(memberno, sellno, optionno);
+        return r > 0 ? "success" : "fail";
+    }
+}
+
---
+++ nextUs/src/main/java/kr/co/nextus/cart/CartServiceImpl.java
@@ -0,0 +1,34 @@
+package kr.co.nextus.cart;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.nextus.selllist.SellListMapper;
+import kr.co.nextus.selllist.SellListVO;
+
+@Service
+public class CartServiceImpl implements CartService {
+	
+	@Autowired
+	private CartMapper mapper;
+	
+	@Override
+	public int insert(int memberno, int sellno, int optionno) {
+		int r = mapper.insert(memberno, sellno, optionno);
+		return r;
+	}
+	
+	@Override
+	public List<CartVO> list(int memberno, int sellno, int optionno) {
+		List<CartVO> data = mapper.list(memberno, sellno, optionno);
+		return data;
+	}
+
+	@Override
+    public int delete(int memberno, int sellno, int optionno) {
+        return mapper.delete(memberno, sellno, optionno);
+    }
+
+}
