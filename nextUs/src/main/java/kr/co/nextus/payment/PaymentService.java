f4192474c2b4022e390a77db6435c7cadbfbdd3b
양재혁 <yjh5128@naver.com>
2024-06-19 16:53:49 +0900

    240619 결제 service 수정

--- nextUs/src/main/java/kr/co/nextus/payment/PaymentService.java
+++ nextUs/src/main/java/kr/co/nextus/payment/PaymentService.java
@@ -2,7 +2,6 @@ package kr.co.nextus.payment;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface PaymentService {
 	void insert(PaymentVO vo);
 	void couponUse(PaymentVO vo);
--- nextUs/src/main/java/kr/co/nextus/payment/PaymentServiceImp.java
+++ nextUs/src/main/java/kr/co/nextus/payment/PaymentServiceImp.java
@@ -1,7 +1,9 @@
 package kr.co.nextus.payment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentServiceImp implements PaymentService {
 	
 	@Autowired
