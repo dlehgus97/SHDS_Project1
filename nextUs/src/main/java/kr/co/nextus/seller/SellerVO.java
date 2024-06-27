88b35320b6764cf9436c76173f542edfa9a7bbdc
hanjucoding <tony990205@naver.com>
2024-06-27 11:51:17 +0900

    셀러 제재 해제 가능

--- nextUs/src/main/java/kr/co/nextus/seller/SellerVO.java
+++ nextUs/src/main/java/kr/co/nextus/seller/SellerVO.java
@@ -49,4 +49,19 @@ public class SellerVO {
 	private String details;
 	private String file_real;
 	private String file_org;
+	
+	// 관리자페이지에서 씁니다요
+	// 사용자로부터 전송되어지는 값(검색, 페이징, 필터링(조건))
+	private String searchType;
+	private String searchWord;
+	private int page; // 사용자가 요청한 페이지 번호
+	private int startIdx; // limit 앞에 들어갈 시작인덱스값
+
+	public SellerVO() {
+		this.page = 1;
+	}
+
+	public int getStartIdx() {
+		return (page - 1) * 8;
+	}
 }
